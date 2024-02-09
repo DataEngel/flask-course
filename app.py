3417bf49800114b6d18974352a991ee236fae49e
Miguel Angel Velazquez Romero <dataengel@Miguels-MacBook-Air.local>
2024-02-08 23:32:20 -0600

    store

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request 
+
 app = Flask(__name__)
 
 stores = [
@@ -12,7 +13,13 @@ stores = [
         ]
     }
 ]
-
-@app.get("/store")
+@app.get("/store") # http://127.0.0.1:5000/store
 def get_stores(): 
     return {"stores": stores} 
+
+@app.post("/store")
+def create_store(): 
+    request_data = request.get_json()
+    new_store = {"name": request_data["name"], "items": []}
+    stores.append(new_store)
+    return new_store, 201
