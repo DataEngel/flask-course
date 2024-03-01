4c4bf48621b1d24a87ad80f0237ab5842055a0fb
DataEngel <miguel.program.73@gmail.com>
2024-02-29 23:20:06 -0600

    store part 2

--- app.py
+++ app.py
@@ -33,3 +33,13 @@ def create_item(name):
             store["items"].append(new_item) 
             return new_item, 201
     return {"message": "Store not found"}, 404 
+
+@app.post("/store/<string:name>")
+def create_item(name):
+    request_data = request.get_json()
+    for store in stores: 
+        if store["name"] == name: 
+            new_item = {"name": request_data["name"], "price": request_data["price"]} 
+            store["items"].append(new_item) 
+            return new_item, 201 
+    return {"message": "Store not found"}, 404
