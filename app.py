01fc5f165cffc375548d157044ed5612ab331ea5
DataEngel <miguel.program.73@gmail.com>
2024-03-21 22:24:05 -0600

    almost stop using list to use dics

--- app.py
+++ app.py
@@ -1,38 +1,36 @@
 from flask import Flask, request 
+from db import items, stores
 
 app = Flask(__name__)
 
-stores = [
-    {
-        "name": "My Store", 
-        "items": [
-            {
-                "name": "Chair", 
-                "price": 15.99
-            }
-        ]
-    }
-]
+
 @app.get("/store") # http://127.0.0.1:5000/store
 def get_stores(): 
-    return {"stores": stores} 
+    return {"stores": list(stores.values())}  
 
 @app.post("/store")
 def create_store(): 
-    request_data = request.get_json()
-    new_store = {"name": request_data["name"], "items": []}
-    stores.append(new_store)
-    return new_store, 201
+    store_data = request.get_json()
+    store_id = uuid.uuid4().hex 
+    store = {**store_data, "id": store_id}
+    stores[store_id] = store
+    return store, 201
 
-@app.post("/store/<string:name>}/item")
+@app.post("/item")
 def create_item(name):
-    request_data = request.get_json()
-    for store in stores: 
-        if store["name"] == name:
-            new_item = {"name": request_data["name"], "price": request_data["price"]} 
-            store["items"].append(new_item) 
-            return new_item, 201
-    return {"message": "Store not found"}, 404 
+    item_data = request.get_json()
+    if item_data["store_id"] not in stores: 
+        return {"message": "Store not found"}, 404 
+    
+    item_id = uuid.uuid4().hex 
+    item = {**store_data, "id": store_id}
+    items[item_id] = item
+
+    return item, 201
+
+@app.get("/item")
+def get_all_items():
+    return {"items": list(items.values())} 
 
 @app.post("/store/<string:name>")
 def create_item(name):
@@ -44,12 +42,12 @@ def create_item(name):
             return new_item, 201 
     return {"message": "Store not found"}, 404
 
-@app.get("/store/<string:name>")
-def get_store(name):
-    for store in stores: 
-        if store["name"] == name:
-            return store
-    return {"message": "Store not found"}, 404
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError: 
+        return {"message": "Store not found"}, 404
 
 @app.get("/store/<string:name>/item")
 def get_item_in_store(name):
